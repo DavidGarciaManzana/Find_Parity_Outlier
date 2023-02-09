3de6f919cdf9fdbbb3f10d05c2bcb431763b66c9
David <Davidgarciama@outlook.com>
2023-02-09 12:19:38 -0600

    Make code understandable, maintanable and easy to read

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,44 @@
-// You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
-// The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer 
-//Write a method that takes the array as an argument and returns this "outlier" N.
-
-
-
-let findOutlier = (integers: number[]): number => {
-    let oddMarker = 0;
-    let evenMarker = 0;
-    let lookingForEven = (arr: number[]) => {
-        arr.forEach((int, index) => {
+const findOutlier = (integers: number[]): number => {
+    let oddMarker: number = 0;
+    let evenMarker: number = 0;
+    const lookingForEven = (arr: number[]) => {
+        arr.forEach((int,index: number) => {
             if (integers[index] % 2 == 0) {
                 evenMarker++
             } else {
                 oddMarker++
-            }
-        })
-        return evenMarker < oddMarker ? true : false
-    }
-    let findEven = (arr: number[]): number => {
-        let result = 0;
+            };
+        });
+        return evenMarker < oddMarker ? true : false;
+    };
+
+    const findEven = (arr: number[]): number => {
+        let evenNumber: number = 0;
         for (let element of arr) {
             if (element % 2 == 0) {
-                result += element
-            }
-        }
-        return result
-    }
-    let findOdd = (arr: number[]): number => {
-        let result = 0;
+                evenNumber += element
+            };
+        };
+        return evenNumber;
+    };
+    const findOdd = (arr: number[]): number => {
+        let oddNumber: number = 0;
         for (let element of arr) {
             if (element % 2 != 0) {
-                result += element
-            }
-        }
-        return result
-    }
+                oddNumber += element
+            };
+        };
+        return oddNumber;
+    };
 
-    
+
     if (lookingForEven(integers)) {
-        return findEven(integers)
-
+        return findEven(integers);
     } else {
-        return findOdd(integers)
-
+        return findOdd(integers);
     }
 }
 
-// findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])
-console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])) //11 (the only odd number)
-console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21])) //160 (the only even number)
 
-//1.- Do a for of the array to look if its mostly odd or even numbers   DONE
-//2.- execute a function to return the number desired                   
+// console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])) //11 (the only odd number)
+// console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21])) //160 (the only even number)
